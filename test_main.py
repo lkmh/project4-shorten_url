750bdd5e9efc041b14414086e94fe24d285d8ce5
Louis Koh <louiskoh1@gmail.com>
2022-10-23 14:14:48 +0800

    step2c testing ok

--- test_main.py
+++ test_main.py
@@ -5,10 +5,13 @@ from .main import app
 client = TestClient(app)
 
 
+""" Step 1 """
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
+
+""" Step 2 - DB setup  """
 
 def test_maxusers_fromDB_main():
     response = get_max_userid_users()
