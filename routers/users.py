77c118c4234ed880f53bf291bdae7a79eb5861b1
Louis Koh <louiskoh1@gmail.com>
2022-10-24 23:05:17 +0800

    Update cookie  policy v2

--- routers/users.py
+++ routers/users.py
@@ -40,7 +40,7 @@ def login(user: User, Authorize: AuthJWT = Depends()):
     else:
     # subject identifier for who this token is for example id or username from database
         access_token = Authorize.create_access_token(subject=response)
-        Authorize.set_access_cookies(access_token)
+        Authorize.set_access_cookies(access_token, samesite='none')
         return {"msg": "Successfully login"}
 
 
