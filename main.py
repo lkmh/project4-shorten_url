9f23b74ef0bf53bcc988bafe657be38202737d04
Louis Koh <louiskoh1@gmail.com>
2022-10-25 00:21:53 +0800

    Update jwt cookie  v8

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[whitelist_IP], ##
+    allow_origins=["https://127.0.0.1:8000/"], ##
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
