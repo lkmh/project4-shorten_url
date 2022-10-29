efb2677d4d58dc1c6869bf5b1992eac8fd05ada4
Louis Koh <louiskoh1@gmail.com>
2022-10-29 21:05:45 +0800

    updated whitelist origins

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://127.0.0.1:8000"], ##
+    allow_origins=["https://127.0.0.1:8000","ttps://lkmh.github.io'"], ##
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["Access-Control-Allow-Origin"],
